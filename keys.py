1c9fc55c127bcde66b15a3d9d7c3fc54ec141ee6
AmanDagar01 <120033508+AmanDagar01@users.noreply.github.com>
2025-01-10 17:43:39 +0530

    Create keys.py
    key file for api keys

---
+++ keys.py
@@ -0,0 +1,16 @@
+api = 'your api key'
+secret = 'your secret key'
+
+
+
+# Constants
+SHORT_MA = 10
+LONG_MA = 50
+RSI_PERIOD = 14
+RSI_OVERBOUGHT = 70
+RSI_OVERSOLD = 30
+CAPITAL = 100
+LEVERAGE = 15
+TRADE_ALLOCATION = 0.5
+SYMBOL = 'ETHUSDT'
+QUANTITY = 0.01  # Adjust according to your strategy
